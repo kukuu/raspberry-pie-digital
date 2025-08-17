af171eb77ab502cd36ed62f04517ca78372cbb40
Alexander Adu-Sarkodie <aadusarkodie1@gmail.com>
2025-08-17 22:56:41 +0100

    Update hardware.test.js

--- conveyor-belt-nodejs-JS/test/integration/hardware.test.js
+++ conveyor-belt-nodejs-JS/test/integration/hardware.test.js
@@ -1,37 +1,29 @@
 const { ConveyorSimulator } = require('../../lib/conveyor');
-const { WorkerSystem } = require('../../lib/workers'); // Fixed import
+const { WorkerSystem } = require('../../lib/workers');
 const gpio = require('../../lib/gpio');
+
+// Mock GPIO for all tests
+beforeAll(() => {
+  gpio.mockMode = true;
+});
 
 describe('Hardware Integration', () => {
   let conveyor;
   let testWorkers;
-  
-  beforeAll(() => {
-    // Initialize with mock mode forced for testing
-    gpio.mockMode = true;
-    conveyor = new ConveyorSimulator();
-    testWorkers = [new WorkerSystem()]; // Now using proper constructor
-  });
 
-  afterAll(() => {
-    gpio.cleanup();
+  beforeEach(() => {
+    conveyor = new ConveyorSimulator();
+    testWorkers = [new WorkerSystem()];
+    jest.spyOn(gpio, 'setLed');
   });
 
   test('should activate belt LED on initialization', () => {
-    // Verify mock console output
-    expect(console.log).toHaveBeenCalledWith(
-      expect.stringContaining('Using GPIO mock mode')
-    );
     expect(gpio.setLed).toHaveBeenCalledWith('beltActive', true);
   });
 
   test('should reflect worker states in GPIO', () => {
-    // Mock worker state
     testWorkers[0].assemblyTime = 4; // Simulate assembling
-    
     conveyor.simulateStep(testWorkers);
-    
-    // Verify worker LED was activated
     expect(gpio.setLed).toHaveBeenCalledWith('worker1', true);
   });
 });
